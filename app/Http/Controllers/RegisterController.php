1ae9448f3ece8b001e7946f74d8a909fe34cc336
Allan <com02520@uoeld.ac.ke>
2024-12-02 09:24:46 +0300

    f

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\SchoolModel;
+use Hash;
 
 class RegisterController extends Controller
 {
@@ -13,17 +16,18 @@ class RegisterController extends Controller
     }
     public function Register(Request $request)
     {
-        request()->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|string|min:8|confirmed',
-        'phone_number' => 'required|string|max:15',
-        'school_name' => 'required|string|max:255',]);
+        // request()->validate([
+        // 'name' => 'required|string|max:255',
+        // 'email' => 'required|email|unique:users,email',
+        // 'password' => 'required|string|min:8|confirmed',
+        // 'phone_number' => 'required|string|max:15',
+        // 'school_name' => 'required|string|max:255',]);
 
-
+        $user = new User();
         $user->name = trim($request->name);
         $user->email = trim($request->email);
-        $user->password = Hash::make($request->name);
+        $user->password = Hash::make($request->password);
+        $user->password_confirmation = Hash::make($request->password_confirmation);
         $user->mobile_number =trim($request->mobile_number);
         $user->user_type = 1;
         $user->save();
@@ -33,6 +37,6 @@ class RegisterController extends Controller
         $school->registered_by = trim($request->name);
         $school->mobile_number =trim($request->mobile_number);
         $school->save();
-        // return redirect('admin/admin/list')->with('success', "Admin successfully created");
+        return redirect('admin/admin/list')->with('success', "School successfully created");
     }
 }
